e69dee981bb3cd1fee77b2b57e06a16021c6a0e3
LXL-xianlong <lxl940827@163.com>
2020-04-07 22:24:40 +0800

    练习代码

--- C_Practice_4_3/C_Practice_4_3/test.c
+++ C_Practice_4_3/C_Practice_4_3/test.c
@@ -90,47 +90,47 @@
 //}
 
 //用C语言在屏幕上输出菱形图案：
-//int main()
-//{
-//	int line = 0;
-//	int i = 0;
-//	scanf("%d", &line);
-//					   
-//	for (i = 0; i<line; i++)
-//	{
-//		//打印一行
-//		//打印空格
-//		int j = 0;
-//		for (j = 0; j<line - 1 - i; j++)
-//		{
-//			printf(" ");
-//		}
-//		//打印*
-//		for (j = 0; j<2 * i + 1; j++)
-//		{
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//
-//
-//	//打印下半部分
-//	for (i = 0; i<line - 1; i++)
-//	{
-//		//打印一行
-//		int j = 0;
-//		for (j = 0; j <= i; j++)
-//		{
-//			printf(" ");
-//		}
-//		for (j = 0; j<2 * (line - 1 - i) - 1; j++)
-//		{
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//	return 0;
-//}
+int main()
+{
+	int line = 0;
+	int i = 0;
+	scanf("%d", &line);
+					   
+	for (i = 0; i<line; i++)
+	{
+		//打印一行
+		//打印空格
+		int j = 0;
+		for (j = 0; j<line - 1 - i; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j<2 * i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+	//打印下半部分
+	for (i = 0; i<line - 1; i++)
+	{
+		//打印一行
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j<2 * (line - 1 - i) - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
 
 
 //求出0～100000之间的所有“水仙花数”并输出。
